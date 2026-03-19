b378f77ded817292fb493e1021cd266aa837a50e
Claudio <work@fleisch-mann.ch>
2026-03-19 01:21:18 +0100

    Client: stuff

---
+++ client/src/pages/ImportedFilesPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageTemplate from "../components/PageTemplate";
+
+const HomePage: React.FC = () => {
+  return (
+    <PageTemplate title="Imported Files">
+      {/* TODO */}
+      <></>
+    </PageTemplate>
+  );
+};
+
+export default HomePage;
