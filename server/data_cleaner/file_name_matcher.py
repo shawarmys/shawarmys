2c51a0e86f0ee73856fbe52d3760fb3061d9d44f
tristan25 <tristan@algomia.com>
2026-03-19 17:53:36 +0100

    Make File_keywords constant

--- server/data_cleaner/file_name_matcher.py
+++ server/data_cleaner/file_name_matcher.py
@@ -1,8 +1,9 @@
+FILE_KEYWORDS = ["device", "device_1hz", "epaAC", "icd", "labs", "medication", "nursing"]
+
 class FileNameMatcher:
     @staticmethod
     def match_file_name(file_name: str) -> str:
-        keywords = ["device", "device_1hz", "epaAC", "icd", "labs", "medication", "nursing"]
-        for keyword in keywords:
+        for keyword in FILE_KEYWORDS:
             if keyword in file_name.lower():
                 return keyword
         return None
