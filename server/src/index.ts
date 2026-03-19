deb4f1a5ba703947302225df256f834c160f9276
Claudio <work@fleisch-mann.ch>
2026-03-19 21:36:58 +0100

    get some help. remove CORS :)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,3 @@
-import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import { apiRouter } from "./router";
@@ -7,17 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(helmet());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://127.0.0.1:3000",
-      "http://localhost:80",
-      "http://127.0.0.1:80",
-    ],
-    credentials: true,
-  }),
-);
 app.use(express.json());
 
 app.use("/api", apiRouter);
